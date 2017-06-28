ab90f91ac3855cdbafb16c904c689381907ab27c
DESKTOP-EKM6MII\lenovo <ddt.narita@gmail.com>
2017-06-28 11:31:26 +0900

    新規コミット　演習01-02　（進捗率100％）作成者　成田修之

---
+++ e_01_02/src/e_01_02/Name.java
@@ -0,0 +1,17 @@
+/* 演習01-02
+ * 各行に1文字ずつ名前を表示するプログラムの作成
+ * 作成日：6月28日
+ * 作成者：成田修之
+ */
+
+package e_01_02;
+
+
+//名前クラス
+class Name {
+	//名前クラスのメインメソッド
+	public static void main(String[] arg) {
+		//成を表示して改行表示
+		System.out.println("成\n田\n修\n之");
+	}
+}
