16e04567aa29d48dcad520678ab9ad59364d52ea
DESKTOP-EKM6MII\lenovo <ddt.narita@gmail.com>
2017-06-28 11:04:44 +0900

    新規コミット　演習01-01　（進捗率100％）作成者　成田修之

---
+++ e_01_01/src/e_01_01/e_01_01.java
@@ -0,0 +1,11 @@
+package e_01_01;
+
+public class e_01_01 {
+	//メインメソッド
+	public static void main(String[] arg) {
+		//セミコロン欠如時の挙動を確認
+		System.out.println("初めてのJavaプログラム。")
+		//セミコロン欠如時の挙動を確認
+		System.out.println("画面に出力しています。")
+	}
+}
