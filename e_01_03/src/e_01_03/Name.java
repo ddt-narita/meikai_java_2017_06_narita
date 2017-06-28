a1e89f454e8aeb67d684d7d61fd856ad41727abc
DESKTOP-EKM6MII\lenovo <ddt.narita@gmail.com>
2017-06-28 11:31:52 +0900

    新規コミット　演習01-03　（進捗率100％）作成者　成田修之

---
+++ e_01_03/src/e_01_03/Name.java
@@ -0,0 +1,17 @@
+/* 演習01-03
+ * 各行に1文字ずつ姓と名の間は一段落開けて作成
+ * 作成日：6月27日
+ * 作成者：成田修之
+ */
+
+package e_01_03;
+
+//名前を表示する表示クラス
+public class Name {
+	//メインメソッド
+	public static void main(String[] args) {
+		//苗字と名前の間に改行を入れて名前を表示
+		System.out.println("成\n田\n\n修\n之");
+	}
+
+}
