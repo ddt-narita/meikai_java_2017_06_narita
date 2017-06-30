755694cf9b0274137d0297b5ac8c5d2c971495c8
DESKTOP-EKM6MII\lenovo <ddt.narita@gmail.com>
2017-06-30 14:41:21 +0900

    追加コミット　演習03-02　（コメント追加）作成者　成田修之

--- e_03_02/src/e_03_02/Divisor.java
+++ e_03_02/src/e_03_02/Divisor.java
@@ -12,20 +12,30 @@ import java.util.Scanner;
 public class Divisor {
 
 	public static void main(String[] args) {
+		//標準出力ストリームのインスタンスを作成
 		Scanner stdIn = new Scanner(System.in);
-		
+
+		//割られるほうの整数Aに入力を促す
 		System.out.print("整数A：");
+		//整数Aにキーボードから入力
 		int nDividedA = stdIn.nextInt();
-		
+
+		//割るほうの整数Bに入力を促す表示
 		System.out.print("整数B：");
+		//整数Bにキーボードから入力
 		int nDivisorB = stdIn.nextInt();
-		
+
+		//整数Aを整数Bで割った剰余が0の時
 		if(nDividedA % nDivisorB == 0) {
+			//BがAの約数であることを表示
 			System.out.print("BはAの約数です。");
+		//剰余が0以外の時
 		} else {
+			//約数ではないことを表示
 			System.out.print("BはAの約数ではありません。");
 		}
-		
+
+		//標準出力ストリームをクローズ
 		stdIn.close();
 	}
 
