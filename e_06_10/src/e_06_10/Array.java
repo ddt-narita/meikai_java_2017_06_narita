827951d59156ea19f32bf4c5e11d75d8214abd42
DESKTOP-EKM6MII\lenovo <ddt.narita@gmail.com>
2017-06-30 18:25:42 +0900

    新規コミット　演習06-10　（進捗率100％）作成者　成田修之

---
+++ e_06_10/src/e_06_10/Array.java
@@ -0,0 +1,53 @@
+/* 演習06-10
+ * 連続する要素が同じ値を持つことのないように演習06-09を改良
+ * 作成日：6月30日
+ * 作成者：成田修之
+ */
+
+
+package e_06_10;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Array {
+
+	public static void main(String[] args) {
+		//標準出力ストリームのインスタンスを生成
+		Scanner stdIn = new Scanner(System.in);
+		//ランダムな値を生成するクラスのインスタンスを生成する
+		Random rand = new Random();
+
+		//配列の要素数の入力を促す
+		System.out.print("配列の要素数：");
+		//配列の要素数を入力
+		int nArrayNumber = stdIn.nextInt();
+
+		//入力された要素数分の配列を確保
+		int[] nArray = new int[nArrayNumber];
+
+		//一つ目の配列は影響を受けないのでそのままランダムな値を代入
+		nArray[0] = rand.nextInt(10) + 1;
+
+		//配列の2番目から最後まで繰り返す
+		for(int i = 1; i < nArrayNumber; i++) {
+			do{
+				//各要素に1から10までの値を入力する
+				nArray[i] = rand.nextInt(10) + 1;
+			//一個手前の値と同じである限り繰り返す
+			}while(nArray[i] == nArray[i - 1]);
+
+		}
+
+		//配列の要素数分繰り返す
+		for(int i = 0; i < nArrayNumber; i++) {
+			//各要素の値を表示する
+			System.out.println("nArray[" + i + "] = " + nArray[i]);
+		}
+
+		//標準出力ストリームをクローズ
+		stdIn.close();
+
+	}
+
+}
