4d96912ca6fbfbb44f4f6d43001c2b82a04e47b4
DESKTOP-EKM6MII\lenovo <ddt.narita@gmail.com>
2017-07-03 15:55:04 +0900

    新規コミット　演習07-05　（進捗率100％）作成者　成田修之

---
+++ e_07_05/src/e_07_05/Hello.java
@@ -0,0 +1,23 @@
+/* 演習07-05
+ * 「こんにちは。」と表示して改行するメソッドhelloの作成
+ * 作成日：7月3日
+ * 作成者：成田修之
+ */
+
+package e_07_05;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		//挨拶のメソッドhelloを呼び出して「こんにちは。」と表示
+		hello();
+	}
+
+	//挨拶して改行するメソッド
+	static void hello() {
+		//「こんにちは。」と表示して開業する
+		System.out.println("「こんにちは。」");
+	}
+
+}
