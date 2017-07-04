facff8e368c7b94c58a36476800d4dd315aed887
DESKTOP-EKM6MII\lenovo <ddt.narita@gmail.com>
2017-07-04 11:10:11 +0900

    追加コミット　演習07-06　（コメントの追加）作成者　成田修之　

--- e_07_06/src/e_07_06/Season.java
+++ e_07_06/src/e_07_06/Season.java
@@ -11,33 +11,44 @@ import java.util.Scanner;
 public class Season {
 
 	public static void main(String[] args) {
-		
+
+		//標準出力ストリームのインスタンスを生成
 		Scanner stdIn = new Scanner(System.in);
-		
+
+		//判定したい月の入力を促す
 		System.out.print("季節を判定したい月を入力してください：");
+		//判定したい月を入力
 		int nMonthSeason = stdIn.nextInt();
-		
+
+		//入力された月をメソッドprintSeasonを使って季節を判定する
 		System.out.print("それは");
+		//メソッドprintSeasonは返却値がvoid型で判定した季節をそのまま表示するので分けて表示
 		printSeason(nMonthSeason);
 		System.out.println("です");
-		
+
+		//標準出力ストリームをクローズ
 		stdIn.close();
 	}
 
-	
+
 	static void printSeason(int m) {
+		//引数の値で分岐
 		switch(m) {
-		case 3:  
-		case 4:  
+		//3月から5月は春であることを表示
+		case 3:
+		case 4:
 		case 5:  System.out.print("春"); break;
-		case 6:  
-		case 7:  
+		//6月から8月は夏であることを表示
+		case 6:
+		case 7:
 		case 8:  System.out.print("夏"); break;
-		case 9:  
-		case 10: 
+		//9月から11月は秋であることを表示
+		case 9:
+		case 10:
 		case 11: System.out.print("秋"); break;
-		case 12: 
-		case 1:  
+		//12月から2月は冬であることを表示
+		case 12:
+		case 1:
 		case 2:  System.out.print("冬"); break;
 		}
 	}
